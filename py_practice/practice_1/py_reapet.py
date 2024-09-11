c8f25aeaf4aa2cfc38cf0c75a71c202fa3bf2a9a
Your Name <you@example.com>
2024-09-12 00:02:19 +0200

    Add pythone examples

---
+++ py_practice/practice_1/py_reapet.py
@@ -0,0 +1,43 @@
+#STRINGS
+
+expstr=" Python is good "
+#prints from 2nd index to end
+
+
+#prints string twice
+
+
+#concatenates string with "test" string
+
+
+#removes leading and trailing whitespaces
+
+
+#print type of variable
+
+
+#print the letters using loop
+
+
+#LISTS
+phrase = "Get the bags right now and some clothes"
+print(phrase)
+#change "phrase" to list and print
+
+
+#add 4 numbers to this list
+
+
+#delete the  char from list on index 2, pop()
+
+
+#remove the first occurrence of 'e' element  
+
+
+#extend the list by list ['e', 'n', 'd']
+
+
+#insert on index 0, the  element which was removed from inedex 3 //use pop(3) and insert function  #plist=insert(2,plist.pop(3))
+
+
+#join the list to a phrase
--- py_string/string_example.py
+++ py_string/string_example.py
@@ -13,3 +13,24 @@ for i in range(2,20,2):
     print(i)
     found.append(random.randint(1,60))
 print(found)
+
+#czy obiejst jest czy nie
+voles = ['a', 'b', 'c', 'd', 'i', 'o', 'u']
+word = "Milliard"
+
+for letter in word:
+    if letter not in voles:
+        print(f"{letter} is not in {voles}")
+        voles.append(letter)
+print(voles)
+
+#opracje na tablicy
+volesexam = ['a', 'b', 'c', 'd', 'i', 'o', 'u']
+
+print(volesexam)
+volesexam.pop()
+print(volesexam)
+volesexam.extend(['z','i'])
+print(volesexam)
+volesexam.insert(5,'a')
+print(volesexam)
